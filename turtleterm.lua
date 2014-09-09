b1d67e5017ee06c80c71f2ce4dc407761b1ad09b
mksas <mathias.husted@live.dk>
2014-09-09 18:31:06 +0200

    Create turtleterm.lua

---
+++ turtleterm.lua
@@ -0,0 +1,10 @@
+--Turtle Term (System)
+--Lizenz: The BSD 3-Clause License
+ 
+print("Turtle Term (Beta)")
+print("Licensed under The BSD 3-Clause License")
+ 
+while true do
+  io.write("> ")
+  --Actions
+end
