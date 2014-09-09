4ef55e9e8d7d4afcea86651542b4aecf33b38840
mksas <mathias.husted@live.dk>
2014-09-09 19:24:28 +0200

    Update turtleterm.lua
    Neue Funktionen.

--- turtleterm.lua
+++ turtleterm.lua
@@ -6,15 +6,17 @@ print("Licensed under The BSD 3-Clause License")
 
 --while true Loop
 while true do
-  io.write("> ")
+  io.write("-> ")
   --Get Input
   input = io.read()
   --Actions von Input
   if input == "exit" then break
   elseif input == "help" then --Muss noch gemacht werden
+  elseif input == "clear" then shell.run("clear")
   elseif input == "w" or input == "W" then turtle.forward()
   elseif input == "a" or input == "A" then turtle.turnLeft()
   elseif input == "s" or input == "S" then turtle.turnRight()
   elseif input == "d" or input == "D" then turtle.right()
+  else print("Ungültiger Befehl")
   end
 end
