6fbe31e6bee9e4caeddb7e15c2452ebc4ef17d18
mksas <mathias.husted@live.dk>
2014-09-10 19:54:44 +0200

    Update turtleterm.lua
    Neue Funktionen.

--- turtleterm.lua
+++ turtleterm.lua
@@ -7,7 +7,7 @@ print("Licensed under The BSD 3-Clause License")
 
 --while true Loop
 while true do
-  io.write("-> ")
+  io.write("- ")
   --Get Input
   input = io.read()
   --Actions von Input
@@ -18,6 +18,12 @@ while true do
   elseif input == "a" or input == "A" then turtle.turnLeft()
   elseif input == "s" or input == "S" then turtle.turnRight()
   elseif input == "d" or input == "D" then turtle.right()
+  elseif input == "dig" or input == "dig" then turtle.dig()
+  elseif input == "dig w" or input == "dig W" then turtle.digUp()
+  elseif input == "dig s" or input == "dig S" then turtle.digDown()
+  elseif input == "place" then turtle.place()
+  elseif input == "place s" or input == "place S" then turtle.placeDown()
+  elseif input == "place w" or input == "place W" then turtle.placeUp()
   elseif input == "refuel" or input  == "load" then shell.run("refuel")
   elseif input == "reboot" or input == "restart" then shell.run("reboot")
   elseif input == "shutdown" or input == "halt" then shell.run("shutdown")
