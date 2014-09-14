5e437b3997ad359de54f90026964ab468c76dc29
mksas <mathias.husted@live.dk>
2014-09-14 13:57:20 +0200

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,9 +1,13 @@
 local arg = {...}
-if arg[1] then arg[1] = arg[1] - 1 end
+if arg[1] then
+  arg[1] = arg[1] - 1
+else
+  turtle.forward()
+end
 if arg[1] > 0 then
   for i=0,arg[1] do
     turtle.forward()
   end
 else
-  turtle.forward()
+  print([[Usage: w [Blocks]]])
 end
