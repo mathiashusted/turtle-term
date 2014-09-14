379adffaba7f33da654a9a5cebfe87c60e03ec31
mksas <mathias.husted@live.dk>
2014-09-14 14:43:08 +0200

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,9 +1,9 @@
 local arg = {...}
-if arg[1] then arg[1] = arg[1] + 0 end
- 
-if arg[1] == nil then arg[1] = 1 end
- 
-if arg[1] > 0 then
+if arg[1] then arg[1] = arg[1] - 1 end
+
+if arg[1] == nil then arg[1] = 0 end
+
+if arg[1] > -1 then
   for i=0,arg[1] do
     turtle.back()
   end
