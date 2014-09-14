3b59ea50635772bb42ca3fbb040d199ebd98cd79
mksas <mathias.husted@live.dk>
2014-09-14 14:59:54 +0200

    Update dig.lua

--- dig.lua
+++ dig.lua
@@ -3,7 +3,7 @@ if arg[1] then
   if arg[1] == "w" or arg[1] == "" then turtle.digUp()
   elseif arg[1] == "s" or arg[1] == "S" then turtle.digDown()
   else
-    print([[Usage: dig [w, s\]]])
+    print([[Usage: dig [w, s] ]])
   end
 else
   turtle.dig()
