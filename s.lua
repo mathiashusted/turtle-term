ad361d9192b8a2f1d307c955676cc83720b0de8e
mksas <mathias.husted@live.dk>
2014-09-14 13:57:48 +0200

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,9 +1,13 @@
 local arg = {...}
-if arg[1] then arg[1] = arg[1] - 1 end
+if arg[1] then
+  arg[1] = arg[1] - 1
+else
+  turtle.back()
+end
 if arg[1] > 0 then
   for i=0,arg[1] do
     turtle.back()
   end
 else
-  turtle.back()
+  print([[Usage: s [Blocks]]])
 end
