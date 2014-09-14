e8a517b6b7d9d10a451d7ec1852ac0fc9343ab91
mksas <mathias.husted@live.dk>
2014-09-14 12:30:03 +0200

    Update dig.lua

--- dig.lua
+++ dig.lua
@@ -1,1 +1,10 @@
-test
+local arg = {...}
+if arg[1] then
+  if arg[1] == "w" or arg[1] == "" then turtle.digUp()
+  elseif arg[1] == "s" or arg[1] == "S" then turtle.digDown()
+  else
+    print([[Usage: dig [w, s]]])
+  end
+else
+  turtle.dig()
+end
